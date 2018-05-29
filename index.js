4564bc2e8329cab9f9611997d9e92a042e189e36
Juan Tenezaca <tenj34@gmail.com>
2018-05-29 21:23:17 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+//1
+function shout(string){
+  return string.toUpperCase()
+}
+
+//2
+function whisper(string){
+return string.toLowerCase()
+}
+
+//3
+function logShout(string){
+  
+}
+
+function sayHiToGrandma (string){ 
+
+}
+
+
